e5b18ec6943ac6fdb143849044bf7ab5816d6592
grussell86 <43388690+grussell86@users.noreply.github.com>
2018-09-18 18:24:11 -0400

    List Removable USB Drives

--- Bash Scripts/list_removable_usb.sh
+++ Bash Scripts/list_removable_usb.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-REMOVABLE_DRIVES=""
-for _device in /sys/block/*/device; do
-    if echo $(readlink -f "$_device")|egrep -q "usb"; then
-        _disk=$(echo "$_device" | cut -f4 -d/)
-        REMOVABLE_DRIVES="$REMOVABLE_DRIVES $_disk"
-    fi
-done
-echo Removable drives found: "$REMOVABLE_DRIVES"
+echo -n 'Removable Drives: '; \lsblk -o NAME,TYPE,RM | grep disk | egrep '1$' | cut -d' ' -f1 | tr '\n' ' ' ; echo
